48ff2de40360f9023a24b23e131e60dec09d578b
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-10-15 17:18:09 +0530

    integrated react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import Home from './component/Home'
 import './App.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Checkout from './component/Checkout'
 
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/checkout',
+    element: <Checkout/>,
+  },
+])
 
 function App() {
   return (
     <div  className='screen'>
-      <Home/>
+      <RouterProvider router={appRouter} />
     </div>
   )
 }
