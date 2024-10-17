b4be81443d6b81113bb8dea6cb5a9ff00413eae4
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-10-17 18:59:13 +0530

    feat/loader animation

---
+++ src/component/Loader.tsx
@@ -0,0 +1,13 @@
+import zomatoLogo from '../assets/zomato-loader.png'
+
+const Loader = () => {
+  return (
+    <div>
+      <div className="loader">
+            <img className="zomato-loader" src={zomatoLogo} alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Loader
