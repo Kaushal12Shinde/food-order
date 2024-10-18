b7deeb7aa3a0238ba38140ae22e5a03f0f2c1ca2
Kaushal12Shinde <kaushushinde12@gmail.com>
2024-10-18 23:44:31 +0530

    created restaurant card

--- src/component/Restaurants.tsx
+++ src/component/Restaurants.tsx
@@ -1,7 +1,14 @@
 function Restaurants() {
   return (
     <div>
-      
+      <div>
+        <img src="" alt="" />
+        <div className="hotel">
+          <p>Lorem, ipsum dolor.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+          <button>View</button>
+        </div>
+      </div>
     </div>
   )
 }
